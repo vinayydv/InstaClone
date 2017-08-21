f8215e486b55c082b2bdbdaf0ed8ac71d5ab43a0
Vinay yadav <404.vinay@gmail.com>
2017-08-21 18:19:47 +0530

    Added css to feeds page

--- myapp/models.py
+++ myapp/models.py
@@ -6,11 +6,11 @@ from uuid import uuid4
 from paralleldots import set_api_key, get_api_key
 from paralleldots import similarity,emotion, sentiment
 
+#api key for parallel dots sentiment analysis
 set_api_key("rpMuzF79DEc91DyPeTe3Dgs1EZ3CYjDC2YRxRGyQQoo")
 
 
-
-# Create your models here.
+#usermodel to store user details in sign up.
 class UserModel(models.Model):
     email = models.EmailField()
     name = models.CharField(max_length=120)
@@ -23,6 +23,7 @@ class UserModel(models.Model):
         return "<UserModel : %s>"%(self.username)
 
 
+#model to store session token details
 class SessionToken(models.Model):
     user = models.ForeignKey(UserModel)
     session_token = models.CharField(max_length=255)
@@ -33,6 +34,7 @@ class SessionToken(models.Model):
         self.session_token = str(uuid4())
 
 
+#model to store a post details
 class PostModel(models.Model):
     user = models.ForeignKey(UserModel)
     image = models.FileField(upload_to='user_images')
@@ -42,10 +44,12 @@ class PostModel(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     @property
+    #shows the number of like on a post
     def like_count(self):
         return self.likemodel_set.count()
 
     @property
+    #to implement sentiment analysis using parallel dots api
     def caption_review(self):
         r = sentiment(str(self.caption))
         print r
@@ -57,10 +61,12 @@ class PostModel(models.Model):
 
 
     @property
+    #to show comments ona post
     def comments(self):
         return CommentModel.objects.filter(post=self).order_by('-created_on')
 
 
+#stores the details of user who liked the post
 class LikeModel(models.Model):
     user = models.ForeignKey(UserModel)
     post = models.ForeignKey(PostModel)
@@ -68,6 +74,7 @@ class LikeModel(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
+#stores details of comment
 class CommentModel(models.Model):
     user = models.ForeignKey(UserModel)
     post = models.ForeignKey(PostModel)
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+
+
+'''
+           **IMPORTANT**
+
+
+           To send mail
+           The mail functionality will only funtion if the admin is logged into the browser with the email and password assigned below
+           in line 66 and line 69
+           and in line 191 and 194
+           Also google app security must be turned off before continuing this
+'''
+
+
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from datetime import datetime
@@ -18,30 +32,54 @@ import smtplib
 
 # Create your views here.
 
-
+#view for signing up the new user
 def signup_view(request):
     today = datetime.now()
+    '''
+    Check if method is GET or POST    
+    '''
     if request.method == 'GET':
         form = SignupForm()
     if request.method == "POST":
         form = SignupForm(request.POST)
+
+        '''
+        If form is valid get data from form
+        '''
+
         if form.is_valid():
             username = form.cleaned_data['username']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
             name = form.cleaned_data['name']
+            #password changed into cryptogrphic form
             hashed_password = make_password(password)
+            '''
+            Save new user
+            '''
             user = UserModel(name=name, password=make_password(password), email=email, username=username)
             user.save()
-            fromaddr = "srkvkr12@gmail.com"
+
+            '''
+            To send mail 
+            **IMPORTANT**
+            The follwing lines will only funtion if the admin is logged into the browser with the email and password assigned below
+            Also google app security must be turned off before continuing this            
+            '''
+
+            #**************REMOVE FOLLOWING COMMENTS TO SEND MAIL AFTER ENTERING YOUR MAIL ID AND PASSWORD CORRECTLY
+            '''
+
+            fromaddr = "abc@gmail.com"
             toaddr = email
             message = "Sign up completed successfully"
-            password = 'vinay@17'
+            password = 'password'
             server = smtplib.SMTP('smtp.gmail.com:587')
             server.starttls()
             server.login(fromaddr, password)
             server.sendmail(fromaddr, toaddr, message)
             server.quit()
+            '''
             return render(request, 'success.html')
 
     return render(request, 'index.html', {
@@ -51,8 +89,16 @@ def signup_view(request):
     })
 
 
+
+#view for login
 def login_view(request):
     response ={}
+
+    '''
+    Check if the request is GET or POST
+    '''
+
+
     if request.method == 'GET':
         form = LoginForm()
         return render(request, 'login.html', {
@@ -61,6 +107,11 @@ def login_view(request):
     elif request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
+
+            '''
+            GET form data
+            '''
+
             username = form.cleaned_data['username']
             password = form.cleaned_data['password']
             user = UserModel.objects.filter(username = username).first()
@@ -68,7 +119,13 @@ def login_view(request):
             print email
 
             if user:
+                '''
+                If user is valid chack for matching username and password in database
+                '''
                 if check_password(password, user.password):
+                    '''
+                    If user is valid validate session
+                    '''
                     session = SessionToken(user = user)
                     session.create_token()
                     session.save()
@@ -83,31 +140,53 @@ def login_view(request):
     return render(request, 'login.html' , response)
 
 
+
+#view for checking validation
 def check_validation(request):
+    '''
+    Check if their is any session token set in the cookie
+    '''
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
+            '''
+            If valid session is found then return the respective user
+            '''
             return session.user
     else:
         return None
 
 
+#view for defining posts
 def post_view(request):
+    '''
+    Check is user is valid by checking valid session
+    '''
     user = check_validation(request)
+    '''
+    api for parallel dots sentiment analysis
+    '''
     set_api_key("rpMuzF79DEc91DyPeTe3Dgs1EZ3CYjDC2YRxRGyQQoo")
     if user:
         if request.method == 'POST':
             form = PostForm(request.POST, request.FILES)
             if form.is_valid():
+                '''
+                If form is valid then get form data
+                '''
                 image = form.cleaned_data.get('image')
                 caption = form.cleaned_data.get('caption')
                 post = PostModel(user=user, image=image, caption=caption)
                 post.save()
 
-
+                #set path for local image storage
                 path = str(BASE_DIR + '\\' + post.image.url)
 
+                '''
+                Set client by providing ingur token id
+                '''
                 client = ImgurClient('4824a0a73eb0084', '66b335766fb74c12d90df2aed8e977f7c1a50992')
+                #push image to the url
                 post.image_url = client.upload_from_path(path,anon=True)['link']
                 post.save()
                 print 'Post is created successfully'
@@ -122,11 +201,22 @@ def post_view(request):
         return redirect('/login/')
 
 
+#view for feeds
 def feed(request):
+    '''
+    Check if user has valid seeion or not
+    '''
+
     user = check_validation(request)
     if user:
-        posts = PostModel.objects.all().order_by('-created_on')[:1]#.filter(user=user)
+        '''
+        If user is valid then get posts from post model
+        '''
+        posts = PostModel.objects.all().order_by('-created_on')[:3]#.filter(user=user)
 
+        '''
+        check if a post is liked by the logged in user or not. If not than show the like button if liked then show the unlike button
+        '''
         for post in posts:
             existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
             if existing_like:
@@ -137,7 +227,11 @@ def feed(request):
         return redirect('/login/')
 
 
+#function to like a post
 def like_view(request):
+    '''
+    Check of a user is valid or not by checking valid session
+    '''
     user = check_validation(request)
     if user and request.method == 'POST':
         form = LikeForm(request.POST)
@@ -146,15 +240,26 @@ def like_view(request):
             existing_like = LikeModel.objects.filter(post_id=post_id, user=user).first()
             if not existing_like:
                 LikeModel.objects.create(post_id=post_id, user=user)
-                fromaddr = "srkvkr12@gmail.com"
+
+                '''
+                         To send mail 
+                         **IMPORTANT**
+                         The follwing lines will only funtion if the admin is logged into the browser with the email and password assigned below
+                         Also google app security must be turned off before continuing this   
+                                  
+                '''
+                #**************REMOVE FOLLOWING COMMENTS TO SEND MAIL AFTER ENTERING YOUR MAIL ID AND PASSWORD CORRECTLY
+                '''
+                fromaddr = "abc@gmail.com"
                 toaddr = user.email
                 message = "You have liked the post successfully"
-                password = 'vinay@17'
+                password = 'password'
                 server = smtplib.SMTP('smtp.gmail.com:587')
                 server.starttls()
                 server.login(fromaddr, password)
                 server.sendmail(fromaddr, toaddr, message)
                 server.quit()
+                '''
                 print 'like row has been successfully created in database'
             else:
                 existing_like.delete()
@@ -162,7 +267,7 @@ def like_view(request):
     else:
         return redirect('/login/')
 
-
+'''
 def feed_view(request):
     user = check_validation(request)
     if user:
@@ -178,9 +283,19 @@ def feed_view(request):
     else:
 
         return redirect('/login/')
+'''
 
 
+#view for comment on a post
 def comment_view(request):
+    '''
+    Check if the user is valid or not by cheking existing session_token
+    if user is valid and request is POST
+    then get comment text from form
+    save comment in comment model
+    :param request:
+    :return:
+    '''
     user = check_validation(request)
     if user and request.method == 'POST':
         form = CommentForm(request.POST)
@@ -198,6 +313,7 @@ def comment_view(request):
         return redirect('/login')
 
 
+#view to invalidating session and logging out the user
 @login_required
 def logout(request):
     auth.logout(request)
